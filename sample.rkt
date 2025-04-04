c60f6c5c673fbaa80450847e6c81557b2d934dc8
Thien Vu <thien.vu@gmail.com>
2025-04-03 22:31:52 -0700

    Include full problem statement

---
+++ sample.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname sample) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (y x) (* x x))
+
+(y 1)
+(y 2)
+(y 3)
+(y 4)
+(y 5)
