5a5589100d1933b0de31dc78f860fb4e12c3b254
Thien Vu <thien.vu@gmail.com>
2025-04-04 23:01:30 -0700

    Exercise 14

---
+++ Exercise 14.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 14|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 14. Define the function string-last, which extracts the last 1String
+; from a non-empty string.
+
+(define (string-last str)
+  (substring str (- (string-length str) 1)))
