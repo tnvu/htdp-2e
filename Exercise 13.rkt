3bb6df69ac6b1d0dd976b0e3970c775314423b4d
Thien Vu <thien.vu@gmail.com>
2025-04-04 22:59:54 -0700

    Exercise 13

---
+++ Exercise 13.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 13|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 13. Define the function string-first, which extracts the first
+; 1String from a non-empty string.
+
+(define (string-first str)
+  (substring str 0 1))
