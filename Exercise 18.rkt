f48a5b85e13a72d95bd589ed47878bfd629c1d0d
Thien Vu <thien.vu@gmail.com>
2025-04-04 23:07:12 -0700

    Exercise 18

---
+++ Exercise 18.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 18|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 18. Define the function string-join, which consumes two strings and
+; appends them with "_" in between. See exercise 2 for ideas.
+
+(define (string-join s1 s2)
+  (string-append s1 "_" s2))
