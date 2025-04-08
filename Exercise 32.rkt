76371db27cbaa5dd4825def2e7319b66fdf10731
Thien Vu <thien.vu@gmail.com>
2025-04-07 23:10:22 -0700

    Exercise 32

---
+++ Exercise 32.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 32|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 32. Most people no longer use desktop computers just to run
+; applications but also employ cell phones, tablets, and their cars’ information
+; control screen. Soon people will use wearable computers in the form of
+; intelligent glasses, clothes, and sports gear. In the somewhat more distant
+; future, people may come with built-in bio computers that directly interact
+; with body functions. Think of ten different forms of events that software
+; applications on such computers will have to deal with.
+
+;  1. glucose monitoring
+;  2. heart rate monitoring
+;  3. context based advertising
+;  4. extreme personalization
+;  5. calendar events
+;  6. real-time traffic
+;  7. automatic language translation
+;  8. sports
+;  9. alarms
+; 10. everything
