5a25f7346d1887d4b191684c8859f9454994a651
Thien Vu <thien.vu@gmail.com>
2025-04-09 23:11:46 -0700

    Exercise 40

---
+++ Exercise 40.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 40|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 40. Formulate the examples as BSL tests, that is, using the check-
+; expect form. Introduce a mistake. Re-run the tests.
+
+; WorldState -> WorldState 
+; moves the car by 3 pixels for every clock tick
+; examples: 
+;   given: 20, expect 23
+;   given: 78, expect 81
+(define (tock cw)
+  (+ cw 3))
+
+(check-expect 23 (tock 20))
+(check-expect 81 (tock 78))
