d8f1edd7f4a5e3b24a64389546752a552c430fd8
Thien Vu <thien.vu@gmail.com>
2025-04-10 23:14:20 -0700

    Exercise 41 Combine Exercises 39-41

---
+++ Exercise 39-41.rkt
@@ -0,0 +1,71 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 39-41|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 39. Good programmers ensure that an image such as CAR can be enlarged
+; or reduced via a single change to a constant definition. Good programmers
+; establish a single point of control for all aspects of their programs, not
+; just the graphical constants. Several chapters deal with this issue. We
+; started the development of our car image with a single plain definition:
+;   (define WHEEL-RADIUS 5)
+; The definition of WHEEL-DISTANCE is based on the wheel’s radius. Hence,
+; changing WHEEL-RADIUS from 5 to 10 doubles the size of the car image. This
+; kind of program organization is dubbed single point of control, and good
+; design employs this idea as much as possible.
+; Develop your favorite image of an automobile so that WHEEL-RADIUS remains the
+; single point of control.
+
+(define WHEEL-RADIUS 5)
+(define WHEEL (circle WHEEL-RADIUS "solid" "black"))
+(define SPACE (rectangle WHEEL-RADIUS (/ WHEEL-RADIUS 2) "solid" "white"))
+(define BOTH-WHEELS (beside WHEEL SPACE WHEEL))
+(define BODY (above (rectangle (* 4 WHEEL-RADIUS) WHEEL-RADIUS "solid" "red")
+                    (rectangle (* 7 WHEEL-RADIUS) (* 2 WHEEL-RADIUS) "solid" "red")))
+(define CAR (underlay/align/offset "middle" "bottom" BODY 0 WHEEL-RADIUS BOTH-WHEELS))
+
+; Exercise 40. Formulate the examples as BSL tests, that is, using the check-
+; expect form. Introduce a mistake. Re-run the tests.
+
+; WorldState -> WorldState 
+; moves the car by 3 pixels for every clock tick
+; examples: 
+;   given: 20, expect 23
+;   given: 78, expect 81
+(define MOVEMENT 3)
+(define (tock cw)
+  (+ cw MOVEMENT))
+
+(check-expect 23 (tock 20))
+(check-expect 81 (tock 78))
+
+; Exercise 41. Finish the sample problem and get the program to run. That is,
+; assuming that you have solved exercise 39, define the constants BACKGROUND and
+; Y-CAR. Then assemble all the function definitions, including their tests. When
+; your program runs to your satisfaction, add a tree to the scenery. We used
+;   (define tree
+;     (underlay/xy (circle 10 "solid" "green")
+;                  9 15
+;                  (rectangle 2 20 "solid" "brown")))
+; to create a tree-like shape. Also add a clause to the big-bang expression that
+; stops the animation when the car has disappeared on the right side.
+
+(define tree (underlay/xy (circle 10 "solid" "green")
+                          9 5
+                          (rectangle 2 20 "solid" "brown")))
+(define BACKGROUND (overlay/align "middle" "middle"
+                                  tree
+                                  (empty-scene (* 10 (image-width CAR)) (* 2 (image-height CAR)))))
+(define Y-CAR (- (image-height BACKGROUND) (/ (image-height CAR) 2)))
+
+(define (render cw)
+  (place-image CAR (- cw (/ (image-width CAR) 2)) Y-CAR BACKGROUND))
+
+(define (end? cw)
+  (> (- cw (image-width CAR) MOVEMENT) (image-width BACKGROUND)))
+
+(define (main ws)
+  (big-bang ws
+    [to-draw render]
+    [on-tick tock]
+    [stop-when end?]))
+
+(main 0)
