27c5ee600d5566bb09ecb2f46fcf400b00dba6e6
Thien Vu <thien.vu@gmail.com>
2025-04-16 23:14:02 -0700

    Exercise 52

---
+++ Exercise 52.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 52|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 52. Which integers are contained in the four intervals above?
+
+; [3, 5] = 3, 4, 5
+; (3, 5] = 4, 5
+; [3, 5) = 3, 4
+; (3, 5) = 4
