13e2fbb56e8f1a0ec3b2d3a3a218ef0f7966f360
Thien Vu <thien.vu@gmail.com>
2025-04-18 23:20:11 -0700

    Exercise 62

---
+++ Exercise 62.rkt
@@ -0,0 +1,56 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 62|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 62. During a door simulation the “open” state is barely visible.
+; Modify door-simulation so that the clock ticks once every three seconds. Rerun
+; the simulation.
+
+; A DoorState is one of:
+; – LOCKED
+; – CLOSED
+; – OPEN
+(define LOCKED "locked")
+(define CLOSED "closed")
+(define OPEN "open")
+   
+; DoorState -> DoorState
+; closes an open door over the period of one tick
+(define (door-closer state-of-door)
+  (cond
+    [(string=? LOCKED state-of-door) LOCKED]
+    [(string=? CLOSED state-of-door) CLOSED]
+    [(string=? OPEN state-of-door) CLOSED]))
+
+(check-expect (door-action LOCKED "u") CLOSED)
+(check-expect (door-action CLOSED "l") LOCKED)
+(check-expect (door-action CLOSED " ") OPEN)
+(check-expect (door-action OPEN "a") OPEN)
+(check-expect (door-action CLOSED "a") CLOSED)
+
+; DoorState KeyEvent -> DoorState
+; turns key event k into an action on state s
+(define (door-action s k)
+  (cond
+    [(and (string=? LOCKED s) (string=? "u" k))
+     CLOSED]
+    [(and (string=? CLOSED s) (string=? "l" k))
+     LOCKED]
+    [(and (string=? CLOSED s) (string=? " " k))
+     OPEN]
+    [else s]))
+
+; DoorState -> Image
+; translates the state s into a large text image
+(check-expect (door-render CLOSED)
+              (text CLOSED 40 "red"))
+(define (door-render s)
+  (text s 40 "red"))
+
+; DoorState -> DoorState
+; simulates a door with an automatic door closer
+(define (door-simulation initial-state)
+  (big-bang initial-state
+    [on-tick door-closer 3]
+    [on-key door-action]
+    [to-draw door-render]))
+;(door-simulation CLOSED)
